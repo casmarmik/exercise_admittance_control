51d4b6133fc92134a2f23ba6cc9cce5363363c89
Jens Møller Rossen <jens.m.rossen@inwatec.dk>
2022-03-31 11:34:33 +0200

    Final working version of Euler Angle Controller

--- EulerAngleController.m
+++ EulerAngleController.m
@@ -3,9 +3,9 @@
 clear
 clc
 
-dt = 0.01; %Timestep
-time = [0:dt:25]; % Time the controller is be simulated in
-torqueTime = [0:dt:5]; % Time the torque is applied
+dt = 0.001; %Timestep
+time = 0:dt:25; % Time the controller is be simulated in
+torqueTime = 0:dt:5; % Time the torque is applied
 
 % Defining the torques applied
 torqueX = zeros(1,5/dt+1) + 1;
@@ -31,15 +31,15 @@ motionProfile = [time; moveXE; moveYE; moveZE]';
 M0 = [2 0 0;
       0 1 0;
       0 0 2];
-K0 = [1.5 0 0;
-      0 0.5 0;
-      0 0 0.1];
+K0 = [4 0 0;
+      0 5 0;
+      0 0 5];
 
-% Defining enviromental stifness. This is what we aim for teh controller to
+% Defining enviromental stifness. This is what we aim for the controller to
 % be critically damped towards
 K = [1 0 0; 0 0.5 0; 0 0 1];
 
 % Calculating D0 based on previously defined matricies and critically
 % damped system, xi = 1 (See lecture 5, slide 34)
-D0 = 2 * sqrt(M0*(K0+K));
-M0inv = inv(M0); 
+D0 = 2 * sqrt(M0*(K0));
+M0inv = inv(M0);
